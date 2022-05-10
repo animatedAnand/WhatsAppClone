1464b772bc21e1d56a5633b759e37d38685eb4c5
anand <kumar.abhayanand19@gmail.com>
2022-05-10 16:58:42 +0530

    Viewpager implemented

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,15 +1,58 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.whatsappclone.adapters.FragmentAdapter;
+import com.example.whatsappclone.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binder;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        binder=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binder.getRoot());
+        auth=FirebaseAuth.getInstance();
+
+        binder.viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        binder.tabLayout.setupWithViewPager(binder.viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+            {
+                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.log_out:
+            {
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this,SignInActivity.class));
+                break;
+            }
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/whatsappclone/SignInActivity.java
+++ app/src/main/java/com/example/whatsappclone/SignInActivity.java
@@ -6,19 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.whatsappclone.databinding.ActivitySignInBinding;
+import com.example.whatsappclone.models.User;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binder;
     private FirebaseAuth auth;
     private ProgressDialog pd;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,7 @@ public class SignInActivity extends AppCompatActivity {
         pd=new ProgressDialog(this);
         pd.setMessage("Logging in..");
         auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
         binder.btSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,10 +72,80 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
             }
         });
+        binder.btGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pd.show();
+                signIn();
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("473963541674-m1r1lston59s7pqt2834kh5ps9so8ft2.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        googleSignInClient= GoogleSignIn.getClient(this,gso);
 
         if(auth.getCurrentUser()!=null)
         {
             startActivity(new Intent(SignInActivity.this,MainActivity.class));
         }
+    }
+    int RC_SIGN_IN=47;
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+            // Signed in successfully, show authenticated UI.
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+
+        //getting user credentials with the help of AuthCredential method and also passing user Token Id.
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+
+        //trying to sign in user using signInWithCredential and passing above credentials of user.
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser cur_user=auth.getCurrentUser();
+                            User user=new User();
+                            user.setUserid(cur_user.getUid());
+                            user.setName(cur_user.getDisplayName());
+                            user.setProfile_picture(cur_user.getPhotoUrl().toString());
+                            database.getReference().child("Users").child(cur_user.getUid()).setValue(user);
+                            // Sign in success, navigate user to Profile Activity
+                            pd.dismiss();
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignInActivity.this, "User authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
